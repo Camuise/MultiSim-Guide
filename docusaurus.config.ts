babfc85eed48cb690922da027db241ccf71689e9
Issac Liu <issac@matchatea.dev>
2025-11-21 11:40:08 -0500

    Update edit URL in docusaurus.config.ts for accurate link to main branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/Camuise/MultiSim-Guide",
+            "https://github.com/Camuise/MultiSim-Guide/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
